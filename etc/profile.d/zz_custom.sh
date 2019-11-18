610ba2a05e7120853725cb94a28128a0efda8600
Cyril Levis <cyril@levis.name>
2019-11-18 20:10:36 +0100

    [cleanup] some random cleans

--- etc/profile.d/zz_custom.sh
+++ etc/profile.d/zz_custom.sh
@@ -21,43 +21,32 @@ export LIBVA_DRIVER_NAME=iHD
 # My own binaries
 export PATH="$HOME/bin:$PATH"
 
+# Fuzzy searching
 export FZ_CMD=j
 export FZ_SUBDIR_CMD=jj
 
+# Video Acceleration
 export LIBVA_DRIVER_NAME=iHD
 
+# Secure lock
 export XSECURELOCK_FONT="-*-open sans-medium-r-*-*-30-*-*-*-*-*-*-uni"
 export XSECURELOCK_SHOW_HOSTNAME=0
 export XSECURELOCK_SHOW_USERNAME=0
 export XSECURELOCK_WANT_FIRST_KEYPRESS=1
 export XSECURELOCK_PASSWORD_PROMPT=time_hex
 
-export WEECHAT_HOME="$HOME/.config/weechat"
-
 
 # Pass configuration
 export PASSWORD_STORE_CHARACTER_SET='a-zA-Z0-9~!@#$%^&*()-_=+[]{};:,.<>?'
 export PASSWORD_STORE_GENERATED_LENGTH=40
 
-# Android configuration
-export ANDROID_SDK_ROOT="$HOME/.android/sdk"
-
-# Haskell configuration
-export PATH="$HOME/.cabal/bin:$PATH"
-
-# Ruby configuration
-if hash ruby 2>/dev/null; then
-  export GEM_HOME="$(ruby -e 'print Gem.user_dir')"
-  export PATH="$GEM_HOME/bin:$PATH"
-fi
 
 export GOPATH="$HOME/.go"
 export PATH="$GOPATH/bin:$PATH"
-export GRAALVM_HOME=/usr/lib/jvm/java-8-graal
 export JAVA_HOME="/usr/lib/jvm/java-8-openjdk"
 export PATH="$HOME/.node_modules/bin:$PATH"
 
 # Nnn files browser settings
 export NNN_BMS='d:~/Vault/Documents;D:~/Downloads/'
 export NNN_TRASH=1
-export NNN_PLUG='d:dragdrop'
+export NNN_PLUG='d:dragdrop;p:paperwork'
