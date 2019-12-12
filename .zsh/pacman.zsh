c87d983443a51d74015a4afc9b9f7058bbab4d52
Cyril Levis <cyril@levis.name>
2019-12-12 19:00:27 +0100

    [zsh] aliases, functions update

--- .zsh/pacman.zsh
+++ .zsh/pacman.zsh
@@ -11,7 +11,6 @@ alias ipacf='SNAP_PAC_SKIP=true pacf'
 alias pacu='pac -Syu'
 alias ipacu='SNAP_PAC_SKIP=true pacu'
 alias pacq='pacman -Si'
-alias pacQ='pacman -Qo'
 alias pacl='pacman -Ql'
 alias pacdiff='sudo \pacdiff; py3-cmd refresh "external_script pacdiff"'
 alias lsp="pacman -Qett --color=always | more"
@@ -28,6 +27,11 @@ pac() {
   rehash
 }
 compdef pac=pacman
+
+
+pacQ() {
+    pacman -Qo `which "$1"`
+}
 
 pacs() {
   aur search -n -k NumVotes "$@"
