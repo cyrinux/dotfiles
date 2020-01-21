a26f67879886261982280befb00035959be3d39a
Cyril Levis <cyril@levis.name>
2020-01-21 22:02:37 +0100

    [nnn] add jump plugin and editor tweak

--- etc/profile.d/zz_custom.sh
+++ etc/profile.d/zz_custom.sh
@@ -54,4 +54,5 @@ export PATH="$HOME/.node_modules/bin:$PATH"
 # Nnn files browser settings
 export NNN_BMS='d:~/Vault/Documents;D:~/Downloads/'
 export NNN_TRASH=1
-export NNN_PLUG='d:dragdrop;p:paperwork;c:croc'
+export NNN_COLORS='4235'
+export NNN_PLUG='j:jump;d:dragdrop;p:paperwork;c:croc'
