f8b13aa78c38d1d3eb31b5b38097d2a28c041235
Cyril Levis <cyril@levis.name>
2021-02-22 09:24:06 +0100

    Update

---
+++ .local/share/qutebrowser/greasemonkey/fingerprint-protector.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @run-at document-start
+// ==/UserScript==
+
+(function () {
+  "use strict";
+
+  const userAgent =
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36";
+  const appVersion = userAgent.substr(8);
+
+  Object.defineProperty(window.navigator, "userAgent", {
+    value: userAgent,
+  });
+  Object.defineProperty(window.navigator, "appVersion", {
+    value: appVersion,
+  });
+  Object.defineProperty(window.navigator, "platform", {
+    value: "Win32",
+  });
+})();
