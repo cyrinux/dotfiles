28fbdd1a87c0f5acd5d4aef85ecb4c2ac8dabd2a
Cyril Levis <git@levis.name>
2022-03-05 16:22:24 +0100

    chore: wip

--- install.sh
+++ install.sh
@@ -179,8 +179,6 @@ mount -o noatime,nodiratime,compress=zstd,subvol=home /dev/mapper/luks /mnt/home
 mount -o noatime,nodiratime,compress=zstd,subvol=pkgs /dev/mapper/luks /mnt/var/cache/pacman
 mount -o noatime,nodiratime,compress=zstd,subvol=aurbuild /dev/mapper/luks /mnt/var/lib/aurbuild
 mount -o noatime,nodiratime,compress=zstd,subvol=archbuild /dev/mapper/luks /mnt/var/lib/archbuild
-mkdir -vp /mnt/home/${user}/.local/share/docker
-mount -o noatime,nodiratime,compress=zstd,subvol=docker /dev/mapper/luks /mnt/home/${user}/.local/share/docker
 mount -o noatime,nodiratime,compress=zstd,subvol=logs /dev/mapper/luks /mnt/var/log
 mount -o noatime,nodiratime,compress=zstd,subvol=temp /dev/mapper/luks /mnt/var/tmp
 mount -o noatime,nodiratime,compress=zstd,subvol=swap /dev/mapper/luks /mnt/swap
@@ -235,7 +233,6 @@ fi
 echo "FONT=$font" > /mnt/etc/vconsole.conf
 echo "KEYMAP=fr" >> /mnt/etc/vconsole.conf
 genfstab -L /mnt >> /mnt/etc/fstab
-sed -i 's|subvol=docker|subvol=docker,user_subvol_rm_allowed|' /mnt/etc/fstab
 echo "${hostname}" > /mnt/etc/hostname
 echo "en_US.UTF-8 UTF-8" >> /mnt/etc/locale.gen
 echo "fr_FR.UTF-8 UTF-8" >> /mnt/etc/locale.gen
@@ -287,7 +284,6 @@ echo "/swap/swapfile none swap defaults 0 0" >> /mnt/etc/fstab
 
 echo -e "\n### Creating user"
 arch-chroot /mnt useradd -m -s /usr/bin/zsh "$user"
-chown -R $user /mnt/home/$user
 for group in wheel network video render plugdev i2c libvirt audit input wireshark rfkill; do
     arch-chroot /mnt groupadd -rf "$group"
     arch-chroot /mnt gpasswd -a "$user" "$group"
