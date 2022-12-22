63c610c10da972caddb53eb13538351988a82f52
Cyril Levis <git@levis.name>
2022-12-22 15:58:21 +0100

    chore: update and fix xplr trash-cli plugin

--- .config/xplr/init.lua
+++ .config/xplr/init.lua
@@ -5,6 +5,4 @@ package.path = os.getenv("XDG_CONFIG_HOME") .. "/xplr/?/init.lua"
 require("ui").setup()
 require("keys").setup()
 require("visit-path").setup()
-
-package.path = os.getenv("XDG_CONFIG_HOME") .. "/xplr/?/src/init.lua"
 require("trash-cli").setup()
