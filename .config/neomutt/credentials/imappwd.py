1a2f5324ef16dd7dca1360ee1cd5f072b3e13e1f
Cyril Levis <git@levis.name>
2022-12-31 19:44:08 +0100

    chore: update

--- .config/neomutt/credentials/imappwd.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-from subprocess import check_output
-
-
-def get_pass(account):
-    return check_output("pass mail/" + account, shell=True).splitlines()[0]
--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -1,20 +1,21 @@
 #!/usr/bin/env zsh
 
-command -v bat           &> /dev/null    && alias c='bat'                                              || alias c='cat'
-command -v lsd           &> /dev/null    && alias la='ll -a'                                           || alias la='ll -A'
-command -v lsd           &> /dev/null    && alias lk='ll -s=size'                                      || alias lk='ll -r --sort=size'
-command -v lsd           &> /dev/null    && alias lm='ll --timesort -r'                                || alias lm='ll -r --sort=time'
-command -v lsd           &> /dev/null    && alias ls='lsd --icon=never'                                || alias ls='ls --color=auto --group-directories-first -h'
-command -v fd            &> /dev/null    && alias fd='fd --hidden --follow'                || alias fd='find . -name'
-command -v git           &> /dev/null    && alias diff='git diff --no-index'
-# command -v htop          &> /dev/null    && alias top='htop'
-command -v dfrs          &> /dev/null    && alias df='dfrs'
-command -v rg            &> /dev/null    && alias rg='rg --hidden --follow --smart-case 2>/dev/null'   || alias rg='grep --color=auto --exclude-dir=.git -R'
-command -v trash-put     &> /dev/null    && alias rm='trash-put'; compdef trash-put=rm
-command -v dog           &> /dev/null    && alias d='dog'                                              || alias d='dig +nocmd +multiline +noall +answer'
-command -v curlie        &> /dev/null    && alias curl='curlie'
-command -v gdu           &> /dev/null    && alias ncdu='gdu'
-command -v lsplug        &> /dev/null    && alias lsusb='lsplug'
+command -v bat &> /dev/null && alias c='bat' || alias c='cat'
+command -v lsd &> /dev/null && alias la='ll -a' || alias la='ll -A'
+command -v lsd &> /dev/null && alias lk='ll -s=size' || alias lk='ll -r --sort=size'
+command -v lsd &> /dev/null && alias lm='ll --timesort -r' || alias lm='ll -r --sort=time'
+command -v lsd &> /dev/null && alias ls='lsd --icon=never' || alias ls='ls --color=auto --group-directories-first -h'
+command -v fd &> /dev/null && alias fd='fd --hidden --follow' || alias fd='find . -name'
+command -v git &> /dev/null && alias diff='git diff --no-index'
+command -v htop &> /dev/null && alias top='htop'
+command -v dfrs &> /dev/null && alias df='dfrs'
+command -v rg &> /dev/null && alias rg='rg --hidden --follow --smart-case 2>/dev/null' || alias rg='grep --color=auto --exclude-dir=.git -R'
+command -v trash-put &> /dev/null && alias rm='trash-put'
+compdef trash-put=rm
+command -v dog &> /dev/null && alias d='dog' || alias d='dig +nocmd +multiline +noall +answer'
+command -v curlie &> /dev/null && alias curl='curlie'
+command -v gdu &> /dev/null && alias ncdu='gdu'
+command -v lsplug &> /dev/null && alias lsusb='lsplug'
 
 man() (
     command man "$@" || "$1" --help || "$1" -h
@@ -27,7 +28,7 @@ alias up='um -p "builtin:tty"'
 
 # Misc
 alias rsync='rsync -rz --info=FLIST,COPY,DEL,REMOVE,SKIP,SYMSAFE,MISC,NAME,PROGRESS,STATS'
-x() { PAGER="less -+F" xplr | IFS= read -r dir && cd -- "${dir:-.}" }
+x() { PAGER="less -+F" xplr | IFS= read -r dir && cd -- "${dir:-.}"; }
 alias n="nmcli"
 alias hex='teehee'
 alias btm='\btm --color gruvbox'
@@ -35,7 +36,6 @@ alias e="$EDITOR"
 alias h="helix"
 alias bottom='btm'
 alias cp='cp -r --reflink=auto'
-alias cpucooling="sudo cpupower frequency-set -u 600Mhz"
 alias battery-full='sudo cctk --PrimaryBattChargeCfg=standard --ValSetupPwd="$(pass personal/bios)"'
 alias battery-express='sudo cctk --PrimaryBattChargeCfg=Express --ValSetupPwd="$(pass personal/bios)"'
 alias battery-normal='sudo cctk --PrimaryBattChargeCfg=custom:50-86 --ValSetupPwd="$(pass personal/bios)"'
@@ -70,69 +70,66 @@ alias tree='LS_COLORS= tree -aC -I .git --dirsfirst'
 alias utc='env TZ="UTC" date'
 alias yubikey-fix='sudo systemctl stop pcscd.service'
 alias zshupdate='z4h update'
-alias shareterm='gotty --permit-write tmux new -A -s gotty sudo docker run -it --rm busybox'
-alias '?'='google'
-
-e64() { [[ $# == 1 ]] && base64 <<<"$1" || base64 }
-d64() { [[ $# == 1 ]] && base64 --decode <<<"$1" || base64 --decode }
-iowaiting() { watch -n 1 "(ps aux | awk '\$8 ~ /D/  { print \$0 }')" }
 alias o=xdg-open
 
+e64() { [[ $# == 1 ]] && base64 <<< "$1" || base64; }
+d64() { [[ $# == 1 ]] && base64 --decode <<< "$1" || base64 --decode; }
+qr() { [[ $# == 1 ]] && qrencode -t utf8 <<< "$1" || qrencode -t utf8; }
+iowaiting() { watch -n 1 "(ps aux | awk '\$8 ~ /D/  { print \$0 }')"; }
+
 # Make directory and enter it
-function md() { [[ $# == 1 ]] && mkdir -p -- "$1" && cd -- "$1" }
+md() { [[ $# == 1 ]] && mkdir -p -- "$1" && cd -- "$1"; }
 compdef _directories md
 
 # Make a temporary directory and enter it
-tmpd() { cd "$(mktemp -d -t "${1:-tmp}.XXXXXXXXXX")" }
+tmpd() { cd "$(mktemp -d -t "${1:-tmp}.XXXXXXXXXX")"; }
 
 # ping test
-p() { [ -n "$DOMAIN" ] && ping ${1}.${DOMAIN} || ping "${1:-8.8.8.8}" }
+p() { [ -n "$DOMAIN" ] && ping ${1}.${DOMAIN} || ping "${1:-8.8.8.8}"; }
 
-iowaiting() { watch -n 1 "(ps aux | awk '\$8 ~ /D/  { print \$0 }')" }
-
+iowaiting() { watch -n 1 "(ps aux | awk '\$8 ~ /D/  { print \$0 }')"; }
 
 # Create a data URL from a file
 dataurl() {
-        local mimeType
-        mimeType=$(file -b --mime-type "$1")
-        if [[ $mimeType == text/* ]]; then
-                mimeType="${mimeType};charset=utf-8"
-        fi
-        echo "data:${mimeType};base64,$(openssl base64 -in "$1" | tr -d '\n')"
+    local mimeType
+    mimeType=$(file -b --mime-type "$1")
+    if [[ $mimeType == text/* ]]; then
+        mimeType="${mimeType};charset=utf-8"
+    fi
+    echo "data:${mimeType};base64,$(openssl base64 -in "$1" | tr -d '\n')"
 }
-
 
 # Show all the names (CNs and SANs) listed in the SSL certificate
 # for a given domain
 getcertnames() {
-        if [ -z "${1}" ]; then
-                echo "ERROR: No domain specified."
-                return 1
-        fi
-        local domain="${1}"
-        echo "Testing ${domain}…"
-        echo ""; # newline
-        local tmp
-        tmp=$(echo -e "GET / HTTP/1.0\\nEOT" \
-                | openssl s_client -connect "${domain}:443" 2>&1)
-        if [[ "${tmp}" = *"-----BEGIN CERTIFICATE-----"* ]]; then
-                local certText
-                certText=$(echo "${tmp}" \
-                        | openssl x509 -text -certopt "no_header, no_serial, no_version, \
+    if [ -z "${1}" ]; then
+        echo "ERROR: No domain specified."
+        return 1
+    fi
+    local domain="${1}"
+    echo "Testing ${domain}…"
+    echo "" # newline
+    local tmp
+    tmp=$(echo -e "GET / HTTP/1.0\\nEOT" |
+        openssl s_client -connect "${domain}:443" 2>&1)
+    if [[ "${tmp}" = *"-----BEGIN CERTIFICATE-----"* ]]; then
+        local certText
+        certText=$(echo "${tmp}" |
+            openssl x509 -text -certopt "no_header, no_serial, no_version, \
                         no_signame, no_validity, no_issuer, no_pubkey, no_sigdump, no_aux")
-                echo "Common Name:"
-                echo ""; # newline
-                echo "${certText}" | grep "Subject:" | sed -e "s/^.*CN=//"
-                echo ""; # newline
-                echo "Subject Alternative Name(s):"
-                echo ""; # newline
-                echo "${certText}" | grep -A 1 "Subject Alternative Name:" \
-                        | sed -e "2s/DNS://g" -e "s/ //g" | tr "," "\\n" | tail -n +2
-                return 0
-        else
-                echo "ERROR: Certificate not found."
-                return 1
-        fi
+        echo "Common Name:"
+        echo "" # newline
+        echo "${certText}" | grep "Subject:" | sed -e "s/^.*CN=//"
+        echo "" # newline
+        echo "Subject Alternative Name(s):"
+        echo "" # newline
+        echo "${certText}" | grep -A 1 "Subject Alternative Name:" |
+            sed -e "2s/DNS://g" -e "s/ //g" | tr "," "\\n" | tail -n +2
+        return 0
+    else
+        echo "ERROR: Certificate not found."
+        return 1
+    fi
 }
 
 ## systemctl
@@ -161,13 +158,20 @@ alias gob='go build'
 alias got='go test'
 alias gotv='go test -v'
 alias goi='go install'
-gov(){
-     go test -coverprofile=coverage.out
-     go tool cover -html=coverage.out
+gov() {
+    go test -coverprofile=coverage.out
+    go tool cover -html=coverage.out
 }
 
 mass-usb-creator() {
-    udevadm monitor | while read -r line ; do key=$(echo $line | sed -n -E 's/^UDEV.*add.*\/block\/(sd[a-z]+) \(block\)$/\1/p'); [ -n "$key" ] && echo "# USB /dev/$key"; { sudo /bin/cp "$1" "/dev/$key" &; sync; } ; done
+    udevadm monitor | while read -r line; do
+        key=$(echo $line | sed -n -E 's/^UDEV.*add.*\/block\/(sd[a-z]+) \(block\)$/\1/p')
+        [ -n "$key" ] && echo "# USB /dev/$key"
+        {
+            sudo /bin/cp "$1" "/dev/$key" &
+            sync
+        }
+    done
 }
 
 # Disable ASLR in a new shell.
@@ -177,7 +181,6 @@ alias unsafeshell='setarch "$(uname -m)" -R /bin/bash'
 alias meteo='curl -s wttr.in/paris'
 
 alias tf=terraform
-qr()  { [[ $# == 1 ]] && qrencode -t utf8 <<<"$1" || qrencode -t utf8 }
 
 go-release() {
     git tag "$(svu next)"
